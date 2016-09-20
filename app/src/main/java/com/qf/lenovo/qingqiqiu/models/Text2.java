d624c8f84ee617ee4be1f6e27c5a647c4f0228b5
dafeige8 <dafeige8@163.com>
2016-09-20 21:38:16 +0800

    dafeige: 3

--- app/src/main/java/com/qf/lenovo/qingqiqiu/models/Text2.java
+++
@@ -1,7 +0,0 @@
-package com.qf.lenovo.qingqiqiu.models;
-
-/**
- * Created by lenovo on 2016/9/20.
- */
-public class Text2 {
-}
--- app/src/main/java/com/qf/lenovo/qingqiqiu/ui/fragments/TravelNoteFragment.java
+++ app/src/main/java/com/qf/lenovo/qingqiqiu/ui/fragments/TravelNoteFragment.java
@@ -2,12 +2,14 @@ package com.qf.lenovo.qingqiqiu.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.framework.magicarena.pulltorefresh.PullToRefreshRecyclerView;
 import com.qf.lenovo.qingqiqiu.R;
+import com.qf.lenovo.qingqiqiu.adapters.TravelNoteAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,14 +20,15 @@ import butterknife.ButterKnife;
 public class TravelNoteFragment extends BaseFragment {
 
     @BindView(R.id.travelnote_recyclerview)
-    PullToRefreshRecyclerView travelnoteRecyclerview;
+    RecyclerView mRecyclerview;
+    private TravelNoteAdapter mAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_travelnote, container, false);
-        ButterKnife.bind(this, layout);
-        return layout;
+        mFragmentLayout = inflater.inflate(R.layout.fragment_travelnote, container, false);
+        ButterKnife.bind(this, mFragmentLayout);
+        return mFragmentLayout;
     }
 
     @Override
@@ -33,10 +36,16 @@ public class TravelNoteFragment extends BaseFragment {
         super.onActivityCreated(savedInstanceState);
 
         initView();
+        setupView();
     }
 
     private void initView() {
 
+        mAdapter = new TravelNoteAdapter(getActivity(),null);
+        mRecyclerview.setAdapter(mAdapter);
+    }
+
+    private void setupView() {
 
     }
 }
