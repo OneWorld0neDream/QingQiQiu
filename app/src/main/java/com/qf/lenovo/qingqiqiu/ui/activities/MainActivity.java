9d4f1238030de329210648d01e36e4a3b8de0ab0
lenovo <337262260@qq.com>
2016-09-20 15:50:30 +0800

    1.3

--- app/src/main/java/com/qf/lenovo/qingqiqiu/ui/activities/MainActivity.java
+++ app/src/main/java/com/qf/lenovo/qingqiqiu/ui/activities/MainActivity.java
@@ -1,18 +1,38 @@
 package com.qf.lenovo.qingqiqiu.ui.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.view.ViewPager;
+import android.widget.EditText;
+import android.widget.TableLayout;
 
 import com.qf.lenovo.qingqiqiu.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.main_search_edit)
+    EditText mainSearchEdit;
+    @BindView(R.id.main_tablayout)
+    TableLayout mainTablayout;
+    @BindView(R.id.main_appbar)
+    AppBarLayout mainAppbar;
+    @BindView(R.id.mian_viewpager)
+    ViewPager mianViewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        inItView();
     }
-    /**
-     * helloEvery
-     */
+
+    private void inItView() {
+
+    }
+
 
 }
