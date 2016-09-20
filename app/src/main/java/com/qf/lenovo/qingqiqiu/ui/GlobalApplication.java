4702adfec82a17280dddd02b77cbfe382fb6b6ac
octivian <everoctivian@gmail.com>
2016-09-20 17:58:33 +0800

    接口情报追加

--- app/src/main/java/com/qf/lenovo/qingqiqiu/ui/GlobalApplication.java
+++ app/src/main/java/com/qf/lenovo/qingqiqiu/ui/GlobalApplication.java
@@ -2,6 +2,8 @@ package com.qf.lenovo.qingqiqiu.ui;
 
 import android.app.Application;
 
+import com.umeng.analytics.MobclickAgent;
+
 import butterknife.ButterKnife;
 
 /**
@@ -12,6 +14,13 @@ public class GlobalApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        //ButterKnifeLibrary
         ButterKnife.setDebug(true);
+
+        //UmengLibrary
+        String appKey = "57e102e9e0f55a31d5001ab2";
+        String channelID = "3q";
+        MobclickAgent.startWithConfigure(new MobclickAgent.UMAnalyticsConfig(this, appKey, channelID, MobclickAgent.EScenarioType.E_UM_GAME_OEM, true));
+
     }
 }
