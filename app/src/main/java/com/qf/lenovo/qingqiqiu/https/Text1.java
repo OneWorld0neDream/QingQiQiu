27bb77a340ee41f86333b106fe5c4f36dba84f1c
lenovo <337262260@qq.com>
2016-09-20 16:07:30 +0800

    1.3

---
+++ app/src/main/java/com/qf/lenovo/qingqiqiu/https/Text1.java
@@ -0,0 +1,7 @@
+package com.qf.lenovo.qingqiqiu.https;
+
+/**
+ * Created by lenovo on 2016/9/20.
+ */
+public class Text1 {
+}
---
+++ app/src/main/java/com/qf/lenovo/qingqiqiu/models/Text2.java
@@ -0,0 +1,7 @@
+package com.qf.lenovo.qingqiqiu.models;
+
+/**
+ * Created by lenovo on 2016/9/20.
+ */
+public class Text2 {
+}
