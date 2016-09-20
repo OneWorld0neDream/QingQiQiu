ce402c94cc32626839703360ec6c07be2907fdc1
lenovo <337262260@qq.com>
2016-09-20 11:58:01 +0800

    1.2

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Practice   
 
 ![IMG](http://img.mp.itc.cn/upload/20160919/35fcc73e8f5d494cbae588c25f9c3af3_th.jpg)
+
+
---
+++ app/src/main/java/com/qf/lenovo/qingqiqiu/ui/activities/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.qf.lenovo.qingqiqiu.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 2016/9/20.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
