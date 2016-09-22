a3aa820376034dac0836f7537fdcb4d87f180d44
octivian <everoctivian@gmail.com>
2016-09-22 11:57:51 +0800

    Last Submit

---
+++ app/src/main/java/com/qf/lenovo/qingqiqiu/ui/activities/ActivitySwitchParams.java
@@ -0,0 +1,11 @@
+package com.qf.lenovo.qingqiqiu.ui.activities;
+
+/**
+ * Created by 31098 on 9/21/2016.
+ */
+
+public interface ActivitySwitchParams {
+    String ACTIVITY_START_PARAM_KEY_MODE = "START_MODE";
+
+    String ACTIVITY_START_PARAM_VALUE_NEARBY = "NEARBY_MORE";
+}
